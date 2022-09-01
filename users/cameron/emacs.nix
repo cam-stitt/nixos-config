d8450e1b3b97590e55579bbbb36d884816ac2659
Cameron Stitt <cameron@cam.st>
2022-09-02 08:25:44 +1000

    Emacs service

--- users/cameron/emacs.nix
+++ users/cameron/emacs.nix
@@ -1,12 +1,21 @@
 { config, pkgs, lib, ... }:
 
+
 with lib;
 {
-    programs.emacs = {
+  programs.emacs = {
+    enable = true;
+    package = ( pkgs.emacs.overrideAttrs (oldAttrs: {
+    configureFlags = oldAttrs.configureFlags ++
+      [ "--without-toolkit-scroll-bars" "--with-x-toolkit=yes" ];
+    }));
+  };
+
+  services.emacs = {
+    enable = true;
+    defaultEditor = true;
+    socketActivation = {
       enable = true;
-      package = ( pkgs.emacs.overrideAttrs (oldAttrs: {
-      configureFlags = oldAttrs.configureFlags ++
-        [ "--without-toolkit-scroll-bars" "--with-x-toolkit=yes" ];
-      }));
     };
+  };
 }
