5b269c6741fbb002dda83bd35808ce6f8022d3c5
Cameron Stitt <cameron@cam.st>
2023-01-17 09:43:31 +1000

    Modify i3 glyphs

--- users/cameron/i3.nix
+++ users/cameron/i3.nix
@@ -24,8 +24,8 @@ let
     one   = "1: ";
     two   = "2: ";
     three = "3: ";
-    four  = "4: ";
-    five  = "5: ";
+    four  = "4: ";
+    five  = "5: ";
     six   = "6: ";
     seven = "7: ";
     eight = "8: " ;
